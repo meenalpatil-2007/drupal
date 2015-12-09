617dc14c2cf57d95feb25421fdca423d0c9422bc
Atif Shaikh <shaikh388@gmail.com>
2015-12-09 16:48:12 +0530

    drupal login changes

--- sites/all/themes/bootstrap/templates/block--user--login.tpl.php
+++ sites/all/themes/bootstrap/templates/block--user--login.tpl.php
@@ -1,4 +1,7 @@
 <?php
+    
+    drupal_add_css('http://drupal.dev/sites/all/themes/bootstrap/css/animate.css', array('group' => CSS_THEME, 'type' => 'external'));
+    drupal_add_css('http://drupal.dev/sites/all/themes/bootstrap/css/signin.css', array('group' => CSS_THEME, 'type' => 'external'));
 
     $elements = drupal_get_form('user_login_block');
 
@@ -12,20 +15,43 @@
     $rendered = drupal_render($elements);
 
     // to see what you have to work with
-    // print "<pre>ELEMENTS: " . print_r($elements,1) . "</pre>";
-
-    $output  = '<form action="' . $elements['#action'] . 
-                              '" method="' . $elements['#method'] . 
-                              '" id="' . $elements['#id'] . 
-                              '" accept-charset="UTF-8"><div>';
-   
-    $output .= $elements['name']['#children'];
-    $output .= $elements['pass']['#children'];
-    $output .= $elements['form_build_id']['#children'];
-    $output .= $elements['form_id']['#children'];
-    $output .= $elements['actions']['#children'];
-    $output .= $elements['links']['#children'];
-    $output .= '</div></form>';
-
-    print 'xxc'.$output;
+    //print "<pre>ELEMENTS: " . print_r($elements) . "</pre>";die;
     ?>
+    <div class="col-md-12 text-center">
+      <h1 class="title-big wow fadeIn"><i class="fa fa-sign-in"></i></h1>
+      <h2 class="subtitle">Sign-in to access your account</h2>
+      <form action="<?php  echo $elements['#action'] ?>" 
+            method="<?php  echo $elements['#method'] ?>" 
+            id    ="<?php  echo $elements['#id'] ?>" 
+            role  ="form"
+            class ="form-signin"
+            accept-charset="UTF-8">                
+   
+        <div class="wow fadeInDown">
+          <label for="inputUsername" class="sr-only"><?php echo $elements['name']['#title'] ?></label>
+          <input class="form-control" placeholder="<?php echo $elements['name']['#title'] ?>" required="required" autofocus="autofocus" id="inputUsername" name="name" type="text">
+        </div>
+
+        <div class="wow fadeInDown" data-wow-delay=".7s">
+          <label for="inputPassword" class="sr-only"><?php echo $elements['pass']['#title'] ?></label>
+          <input class="form-control" placeholder="<?php echo $elements['pass']['#title'] ?>" required="required" id="inputPassword" name="pass" type="password" value="">
+        </div>
+
+        <button class="btn btn-lg btn-primary btn-block wow fadeInUp"  data-wow-delay="1s" id="signin" type="submit"><?php echo $elements['actions']['submit']['#value'] ?></button> 
+      <?php echo $elements['form_build_id']['#children']; ?>
+      <?php echo $elements['form_id']['#children'];?>
+      <?php 
+        $tmp = explode("<ul><li>", $elements['links']['#children']);
+        $tmp1 = explode("</li>", $tmp[1]);
+        print '<div class="links wow fadeInUp btn btn-lg btn-primary btn-block" data-wow-delay="1.5s">'.  $tmp1[0].'</div>';
+        print '<div class="wow fadeInUp btn btn-lg btn-primary btn-block" data-wow-delay="1.5s">'.  $tmp1[1].'</div>';
+
+        //echo '<div class="btn btn-lg btn-primary">'.$tmp[0].'</div>';
+      ?>
+    </form>
+  </div>
+
+<?php
+  drupal_add_js('sites/all/themes/bootstrap/js/wow.js', array('type' => 'file', 'scope' => 'footer'));
+  drupal_add_js('sites/all/themes/bootstrap/js/main.js', array('type' => 'file', 'scope' => 'footer'));
+?>
--- sites/all/themes/bootstrap/templates/system/page.tpl.php
+++ sites/all/themes/bootstrap/templates/system/page.tpl.php
@@ -140,7 +140,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
+        <!--h1 class="page-header"><?php print $title; ?></h1-->
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
@@ -152,7 +152,12 @@
       <?php endif; ?>
       <?php if (!empty($action_links)): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
+      <?php endif; 
+        if (drupal_is_front_page())
+    {
+        unset($page['content']['system_main']['default_message']);
+    } 
+      ?>
       <?php print render($page['content']); ?>
     </section>
 
